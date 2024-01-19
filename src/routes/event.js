81eebd9e01d0cc0f1a0d326ab71e12964d32385d
christianLizama <ckill661@gmail.com>
2024-01-18 21:26:57 -0300

    corrección de middleware para eliminar evento

--- src/routes/event.js
+++ src/routes/event.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res, next) {
 //Rutas para eventos
 router.post(
   "/eliminarEvento",
-  authMiddleware.verifyAdminOrAdminApp,
+  authMiddleware.verfyAdminsOrEditers,
   evento.eliminarEvento
 );
 
